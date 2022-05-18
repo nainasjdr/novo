bfd8db1828fbe384ed7d8e4698931020651de9fe
nainasjdr <nainasjdr@gmail.com>
2022-05-18 16:52:10 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1';// Endereço do servidor
+const port = 3000; // porta do servidor
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+const server = http.createServer((req, res) => { // Criando o servidor
+  res.statusCode = 200; // Status ok de resposta
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Ola Mundo uba');
+  res.end('Ola Mundo uba, janaína');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+  
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
 })
